d8c6b08c3a6756517ee9003145a955020f5082e5
Josh Ryals <jryals@kvc.org>
2023-03-21 13:32:40 -0500

    Added TSDoc comments for clarification

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,29 @@
 declare module "react-activity-detector" {
   import React from "react";
 
+  type ActivityEvent =
+    | "click"
+    | "keydown"
+    | "DOMMouseScroll"
+    | "mousewheel"
+    | "mousedown"
+    | "touchstart"
+    | "touchmove"
+    | "focus";
+
   export interface ActivityDetectorProps {
-    activityEvents?: string[];
+    /**
+     * Array of possible events that trigger user activity
+     * Defaults to all events
+     */
+    activityEvents?: ActivityEvent[];
+    /** Time in milliseconds before user is considered idle */
     timeout?: number;
+    /** Whether to start the detector in an idle state */
     enabled?: boolean;
+    /** Event that triggers when user is declared idle */
     onIdle?: () => void;
+    /** Event that triggers when user is declared active */
     onActive?: () => void;
   }
 
